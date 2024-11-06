a8f1d69a0f76b07dcf6dd7c35042c429e239ff3c
Muhammad Abdullah <ranausama02@gmail.com>
2024-11-06 15:39:27 +0100

    Initial commit for backend

--- routes/books.js
+++ routes/books.js
@@ -15,11 +15,36 @@ router.get("/getData", async (req, res) => {
 });
 
 // Route to search by "Submitted GenBank assembly"
-router.post("/search", async (req, res) => {
-  const { query } = req.body;
+// router.post("/search", async (req, res) => {
+//   const { query } = req.body;
+
+//   try {
+//     const result = await Book.findOne({ "Submitted GenBank assembly": query });
+//     if (result) {
+//       res.status(200).json(result);
+//     } else {
+//       res.status(404).json({ message: "No results found for your query." });
+//     }
+//   } catch (error) {
+//     console.error("Error fetching data:", error);
+//     res.status(500).json({ error: "Could not fetch document", details: error });
+//   }
+// });
+
+// POST /search route to handle searches based on type
+router.post('/search', async (req, res) => {
+  const { query, type } = req.body;
 
   try {
-    const result = await Book.findOne({ "Submitted GenBank assembly": query });
+    let result;
+
+    // Check the type and apply the corresponding search condition
+    if (type === "scientific_name") {
+      result = await Book.findOne({ "Taxon": query });
+    } else if (type === "id") {
+      result = await Book.findOne({ "Submitted GenBank assembly": query });
+    }
+
     if (result) {
       res.status(200).json(result);
     } else {
