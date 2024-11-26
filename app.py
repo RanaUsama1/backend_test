f02205a04c9820579d86694eaad12f7bdeb10b19
Muhammad Abdullah <ranausama02@gmail.com>
2024-11-26 17:27:43 +0100

    Initial commit for backend

--- app.py
+++ app.py
@@ -66,5 +66,8 @@ def search():
     metadata = search_species_data(species_name)
     return jsonify({"species_name": species_name, "metadata": metadata})
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Default to 5000 if PORT is not set
+    app.run(host='0.0.0.0', port=port, debug=True)
